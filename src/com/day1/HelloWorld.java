229dd8886db981cb24b093a764a33352dc924a79
Training <Training@DESKTOP-EKUK0M3>
2022-08-16 01:59:33 +0800

    first commit

---
+++ src/com/day1/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.day1;
+
+import java.util.Date;
+
+public class HelloWorld {
+
+	
+	
+	
+	
+	 public static void main(String[] args ) {
+		 
+		 System.out.println("Hello world testing "+ new Date() );
+		
+
+	}
+	
+	
+	
+
+}
