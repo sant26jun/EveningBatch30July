7b5cb9e31843466d744b0dacfe847a51cdf754f6
Rajeev Kumar Singh <rajkumar888@gmail.com>
2022-08-18 04:07:29 +0800

    hello testing

--- src/com/day1/HelloWorld.java
+++ src/com/day1/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
 	
 	 public static void main(String[] args ) {
 		 
-		 System.out.println("Hello world testing..... "+ new Date() );
+		 System.out.println("Hello world.. testing..... "+ new Date() );
 		
 
 	}
